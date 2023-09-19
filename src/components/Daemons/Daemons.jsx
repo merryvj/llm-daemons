c4b0a437fd2b2cad2be8e08954ced32d4a25c868
Mary Jiang <mvjiang@gmail.com>
2023-09-19 17:54:42 -0400

    dynamically styling options

--- src/components/Daemons/Daemons.jsx
+++ src/components/Daemons/Daemons.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import styles from './daemons.module.css'
 
+const options = [
+    {
+        title: "Editor",
+        description: "This is really strict",
+        color: "#ffaaaa"
+    },
+    {
+        title: "Mom",
+        description: "This is really strict",
+        color: "#aaffaa"
+    },
+    {
+        title: "Friend",
+        description: "This is really strict",
+        color: "#aaaaff"
+    },
+]
 function Daemons() {
   return (
     <div className={styles.wrapper}>
-        <div className={styles.daemon}>
-            <span className={styles.label}>Label</span>
-        </div>
-        <div className={styles.daemon}>
-            <span className={styles.label}>Label</span>
-        </div>
-        <div className={styles.daemon}>
-            <span className={styles.label}>Label</span>
-        </div>
+        {options.map((option, i) => (
+            <div key={i} className={styles.option}>
+                <div className={styles.icon} style={{background: option.color}}/>
+                <span className={styles.label} style={{color: option.color}}>{option.title}</span>
+            </div>
+
+        ))}
     </div>
   )
 }
