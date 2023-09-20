6678262895ce6813afb5573f3f7c424c744eda93
Mary Jiang <mvjiang@gmail.com>
2023-09-19 20:58:30 -0400

    modal appears on hover

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -8,12 +8,9 @@ export default function Editor() {
   const [text, setText] = useState("Those interested in utilizing their Samaritans experience toward this end must enter a volunteer training class in the semester PRIOR to the course which they anticipate applying their hotline experience. Requirements vary by course, program and institution and it is the responsibility of each volunteer to determine how their Samaritans experience will fit in the context of their individual circumstance. Please note, supervision of accrued hours are for the purpose of satisfying individual courses and shorter-term experiences. Samaritans does not provide clinical supervision for practitioner licensing.");
   const [textParts, setTextParts] = useState([]);
   const [edits, loading] = useAI(text);
-  const [modal, setModal] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-  
-  const showModal = (e) => {
-    setModal(true);
-  }
+
 
   const findMatchingSection = (sentence) => {
     const escapedSentence = escapeRegExp(sentence);
@@ -28,7 +25,12 @@ export default function Editor() {
     const parts = text.split(matchingSection);
     const editedText = parts.map((part, i) => (
       i < parts.length - 1 ? 
-      <React.Fragment key={i}>{part}<mark>{matchingSection}</mark></React.Fragment> : 
+      <React.Fragment key={i}>{part}
+        <mark 
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+      >{matchingSection}</mark>
+      </React.Fragment> : 
       part
     ));
     setTextParts(editedText);
@@ -38,8 +40,8 @@ export default function Editor() {
 
   return (
     <div className={styles.wrapper}>
-      <p className={styles.text}>{loading ? textParts.map((part, i) => <React.Fragment key={i} className={styles.text}>{part}</React.Fragment>) : text}</p>
-      {loading && <Modal suggestion={edits.suggestions} visible={modal}/>}
+      <p className={styles.text}>{loading ? textParts.map((part, i) => <React.Fragment key={i}>{part}</React.Fragment>) : text}</p>
+      <Modal suggestion={edits.suggestions} visible={isHovered}/>
     </div>
   )
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './modal.module.css';
 
-export default function Modal({suggestion}) {
+export default function Modal({suggestion, visible}) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} style={{visibility: visible ? 'visible' : 'hidden'}}>
         {suggestion}
     </div>
   )
