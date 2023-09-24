955756befbab0599431327fb00a64d50d51cd440
Mary Jiang <mvjiang@gmail.com>
2023-09-24 16:26:24 -0400

    there are faces!!!

--- src/components/Daemons/Daemons.jsx
+++ src/components/Daemons/Daemons.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './daemons.module.css'
 
 const options = [
@@ -22,11 +22,26 @@ const options = [
     },
 ]
 function Daemons({onSelect}) {
+    const [active, setActive] = useState(null);
+
+    const handleClick = (option) => {
+        onSelect(option)
+    }
+
   return (
     <div className={styles.wrapper}>
         {options.map((option, i) => (
-            <div key={i} className={styles.option} onClick={() => onSelect(option)}>
-                <div className={styles.icon} style={{background: `rgb(${option.color})`}}/>
+            <div key={i} className={styles.option} onClick={() => handleClick(option)}>
+                <div className={styles.icon} style={{"--color": option.color}}>
+                    <div className={styles.face}>
+                        <svg viewBox="0 0 100 100">
+                            <circle cx="30" cy="40" r="7" />
+                            <circle cx="70" cy="40" r="7" />
+                            <path d="M 35 62 C 40 70, 60 70, 65 62" stroke="black" strokeWidth={10} strokeLinecap='round' fill="transparent"/>
+
+                        </svg>
+                    </div>
+                </div>
                 <span className={styles.label} style={{color: `rgb(${option.color})`}}>{option.title}</span>
             </div>
 
